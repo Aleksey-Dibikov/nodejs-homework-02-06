387ffea972131a357d1e6dc4ccd947901bb9a4cb
AlekseyDibikov <79219724+Aleksey-Dibikov@users.noreply.github.com>
2022-01-25 01:48:17 +0200

    adds sendgrid

--- controllers/auth/controllers-auth-index.js
+++ controllers/auth/controllers-auth-index.js
@@ -1,5 +1,10 @@
 import { HttpCode } from '../../libs/constants';
 import authService from '../../service/auth/service-auth-index';
+import {
+    EmailService,
+    SenderSendgrid,
+    SenderNodemailer
+} from '../../service/email';
 
 const registration = async (req, res, next) => {
     try {
@@ -14,13 +19,23 @@ const registration = async (req, res, next) => {
                     message: 'Email in use'
                 })
         }
-        const data = await authService.create(req.body);
+        const userData = await authService.create(req.body);
+        const emailService = new EmailService(
+            process.env.NODE_ENV,
+            new SenderSendgrid(),
+        )
+        const isSend = await emailService.sendVerifyEmail(
+            email,
+            userData.name,
+            userData.verifyTokenEmail
+        )
+        delete userData.verifyTokenEmail
         res
             .status(HttpCode.CREATED)
             .json({
                 status: 'success',
                 code: HttpCode.CREATED,
-                data,
+                data: { ...userData, isSendEmailVerify: isSend },
             })        
     } catch (error) {
         next(error)
--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,10 +1,30 @@
 /* eslint-disable no-unused-vars */
+import repositoryContacts from '../../repository/repository-contacts'
+import repositoryUsers from '../../repository/repository-users'
 import { HttpCode } from "../../libs/constants"
 import {
     UploadFileService,
     LocalFileService,
     CloudFileService
 } from '../../service/file-storage'
+import {
+    EmailService,
+    SenderNodemailer,
+    SenderSendgrid,
+} from '../../service/email'
+
+const aggregation = async (req, res, next) => {
+  const { id } = req.params
+  const data = await repositoryContacts.getStatisticsContacts(id)
+  if (data) {
+    return res
+      .status(HttpCode.OK)
+      .json({ status: 'success', code: HttpCode.OK, data })
+  }
+  res
+    .status(HttpCode.NOT_FOUND)
+    .json({ status: 'error', code: HttpCode.NOT_FOUND, message: 'Not found' })
+}
 
 const uploadAvatar = async (req, res, next) => {
     const uploadService = new UploadFileService(
@@ -18,4 +38,59 @@ const uploadAvatar = async (req, res, next) => {
         .json({ status: 'success', code: HttpCode.OK, data: { avatarUrl } })
 }
 
-export { uploadAvatar }
+const verifyUser = async (req, res, next) => {
+  const verifyToken = req.params.token
+  const userFromToken = await repositoryUsers.findByVerifyToken(verifyToken)
+
+  if (userFromToken) {
+    await repositoryUsers.updateVerify(userFromToken.id, true)
+    return res.status(HttpCode.OK).json({
+      status: 'success',
+      code: HttpCode.OK,
+      data: { message: 'Success' },
+    })
+  }
+  res.status(HttpCode.BAD_REQUEST).json({
+    status: 'success',
+    code: HttpCode.BAD_REQUEST,
+    data: { message: 'Invalid token' },
+  })
+}
+
+const repeatEmailForVerifyUser = async (req, res, next) => {
+  const { email } = req.body
+  const user = await repositoryUsers.findByEmail(email)
+  if (user) {
+    const { email, name, verifyTokenEmail } = user
+    const emailService = new EmailService(
+      process.env.NODE_ENV,
+      new SenderNodemailer(),
+    )
+
+    const isSend = await emailService.sendVerifyEmail(
+      email,
+      name,
+      verifyTokenEmail,
+    )
+    if (isSend) {
+      return res.status(HttpCode.OK).json({
+        status: 'success',
+        code: HttpCode.OK,
+        data: { message: 'Success' },
+      })
+    }
+    return res.status(HttpCode.UE).json({
+      status: 'error',
+      code: HttpCode.UE,
+      data: { message: 'Unprocessable Entity' },
+    })
+  }
+
+  res.status(HttpCode.NOT_FOUND).json({
+    status: 'error',
+    code: HttpCode.NOT_FOUND,
+    data: { message: 'User with email not found' },
+  })
+}
+
+export { aggregation, uploadAvatar, verifyUser, repeatEmailForVerifyUser }
